8823ec98363bac365893e938cc34ca38a15cd2b7
punkish <punk.kish@gmail.com>
2017-12-17 12:09:32 +0530

    removed queries to a separate file

--- api/v1/resources/records.js
+++ api/v1/resources/records.js
@@ -3,11 +3,10 @@ const Schema = require('../schema.js');
 const Config = require('../../../config.js');
 const ResponseMessages = require('../../response-messages');
 const Utils = require('../utils.js');
-
-//const cache = require('persistent-cache');
 const Cache = require('persistent-cache')({
     name: 'records'
 });
+const recordsQueries = require('./records-queries.js');
 
 const records = {
     method: 'GET',
@@ -34,7 +33,12 @@ const records = {
     handler: function(request, reply) {
         let uri = Config.uri + 'records/?communities=biosyslit';
         
-        // construct the 'uri' based on the query params
+        // construct the 'uri' based on the query params that will 
+        // be sent to Zenodo. Note that the following query params 
+        // are NOT sent to Zenodo. They are only for local logic
+        //    request.query.summary
+        //    request.query.refreshCache
+        //    request.query.images
         [
             'q', 
             'file_type', 
@@ -56,159 +60,104 @@ const records = {
         let cacheKey = '';
         if (request.query.summary) {
 
-            cacheKey = Utils.createCacheKey(uri + request.query.summary);
-            Cache.get(cacheKey, function(err, result) {
-                if (err) {
-                    console.log(err);
-                }
+            cacheKey = Utils.createCacheKey(uri + '&summary=' + request.query.summary);
 
-                if (result) {
-                    reply(result);
+            if (request.query.refreshCache) {
+                try {
+                    recordsQueries.getSummaryOfRecords(uri, cacheKey);
                 }
-                else {
-                    const getSummaryOfRecords = async function (uri) {
-                        
-                        const { res, payload } = await Wreck.get(uri);
-                        const summary = JSON.parse(payload.toString())
-                            .hits
-                            .hits
-                            .map(function(element) {
-                                return element.links.self;
-                            });
-                    
-                        Cache.put(cacheKey, summary, function(err) {
-                            if (err) {
-                                console.log(err);
+                catch (error) {
+                    console.error(error);
+                }
+            }
+            else {
+                Cache.get(cacheKey, function(err, result) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        if (result) {
+                            reply(result);
+                        }
+                        else {
+                            try {
+                                recordsQueries.getSummaryOfRecords(uri, cacheKey);
                             }
-
-                            reply(summary).headers = res.headers;
-                        });
-                    };
-    
-                    try {
-                        getSummaryOfRecords(uri);
+                            catch (error) {
+                                console.error(error);
+                            }
+                        }
                     }
-                    catch (error) {
-                        console.error(error);
-                    }
-                }
-            });
+                });
+            }
         }
         else if (request.query.images) {
             
-            cacheKey = Utils.createCacheKey(uri + request.query.images);
-            Cache.get(cacheKey, function(err, result) {
-                if (err) {
-                    console.log(err);
+            cacheKey = Utils.createCacheKey(uri + '&image=' + request.query.images);
+            if (request.query.refreshCache) {
+                console.log('refreshing cache');
+                try {
+                    recordsQueries.getImages(uri, cacheKey, reply);
                 }
-
-                if (result) {
-                    reply(result);
+                catch (error) {
+                    console.error(error);
                 }
-                else {
-                    try {
-                        const getImages = async function(uri) {
-                            
-                            // get all the records for the query
-                            try {
-                                const { res, payload } = await Wreck.get(uri);
-                                
-                                // extract all the links for the records
-                                const records = JSON.parse(payload.toString())
-                                    .hits
-                                    .hits
-                                    .map(function(element) {
-                                        return element.links.self;
-                                    });
-            
-                                //reply(records).headers = res.headers;
-                                // get images for each record
-                                let imagesOfRecords = {};
-                                for (let record of records) {
-                        
-                                    // get images of one record
-                                    
-                                    // first get the bucket for one record
-                                    try {
-                                        const { res, payload } = await Wreck.get(record);
-                                        const bucket = JSON.parse(payload.toString()).links.bucket;
-            
-                                        try {
-                                            // now get the images for the bucket
-                                            const { res, payload } = await Wreck.get(bucket);
-                                            const contents = JSON.parse(payload.toString()).contents;
-                                            const images = contents.map(function(el) { return el.links.self; });
-                                            imagesOfRecords[record] = images;
-                                        }
-                                        catch (error) {
-                                            console.log(error);
-                                        }
-                                    }
-                                    catch (error) {
-                                        console.log(error);
-                                    }
-                                };
-            
-                                Cache.put(cacheKey, imagesOfRecords, function(err) {
-                                    if (err) {
-                                        console.log(err);
-                                    }
-
-                                    reply(imagesOfRecords).headers = res.headers;
-                                });
-                            }
-                            catch (error) {
-                                console.log(error);
-                            } 
-                        };
-            
-                        getImages(uri);
+            }
+            else {
+                Cache.get(cacheKey, function(err, result) {
+                    console.log('checking cache');
+                    if (err) {
+                        console.log(err);
                     }
-                    catch (error) {
-                        console.error(error);
+
+                    if (result) {
+                        console.log('found result in cache');
+                        reply(result);
                     }
-                }
-            });
-        }
-    
-        // return the all the details of all the records
-        else {
-            cacheKey = Utils.createCacheKey(uri);
-            Cache.get(cacheKey, function(err, result) {
-                if (err) {
-                    console.log(err);
-                }
-
-                if (result) {
-                    reply(result);
-                }
-                else {
-                    try {
-                        const getRecords = async function (uri) {
-                            
-                            const { res, payload } = await Wreck.get(uri);
-                            const result = JSON.parse(payload.toString());
-                            Cache.put(cacheKey, result, function(err) {
-                                if (err) {
-                                    console.log(err);
-                                }
-
-                                reply(result).headers = res.headers;
-                            });
-                        };
-                        
+                    else {
+                        console.log('doing a fresh query');
                         try {
-                            getRecords(uri);
+                            recordsQueries.getImages(uri, cacheKey, reply);
                         }
                         catch (error) {
                             console.error(error);
                         }
                     }
-                    catch (error) {
-                        console.error(error);
-                    }
+                });
+            }
+        }
+    
+        // return the all the details of all the records
+        else {
+            cacheKey = Utils.createCacheKey(uri);
+
+            if (request.query.refreshCache) {
+                try {
+                    recordsQueries.getRecords(uri, cacheKey);
                 }
-            });
+                catch (error) {
+                    console.error(error);
+                }
+            }
+            else {
+                Cache.get(cacheKey, function(err, result) {
+                    if (err) {
+                        console.log(err);
+                    }
+
+                    if (result) {
+                        reply(result);
+                    }
+                    else {
+                        try {
+                            recordsQueries.getRecords(uri, cacheKey);
+                        }
+                        catch (error) {
+                            console.error(error);
+                        }
+                    }
+                });
+            }
         }
     }
 };
