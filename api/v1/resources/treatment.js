84fb0976856970de5af55a095a47a8d80f1ca066
punkish <punk.kish@gmail.com>
2018-04-05 16:48:44 +0200

    now serving treatments

---
+++ api/v1/resources/treatment.js
@@ -0,0 +1,63 @@
+const Joi = require('joi');
+const Config = require('../../../config.js');
+const ResponseMessages = require('../../response-messages');
+const Utils = require('../utils.js');
+const Cache = Utils.cache('record');
+const Database = require('better-sqlite3');
+
+const db = new Database('data/tb.sqlite');
+
+const treatment = {
+
+    method: 'GET',
+
+    path: "/treatment/{id}",
+
+    config: {
+        description: "treatment",
+        tags: ['treatment', 'api'],
+        plugins: {
+            'hapi-swagger': {
+                order: 2,
+                responseMessages: ResponseMessages
+            }
+        },
+        validate: {
+            params: {
+                id: Joi.string().required()
+            }
+        },
+        notes: [
+            'This is the main route for fetching a treatment matching an id.'
+        ]
+    },
+    
+    handler: function(request, reply) {
+        const uri = Config.uri + 'treatment/' + encodeURIComponent(request.params.id);
+
+        const cacheKey = Utils.createCacheKey(uri);
+
+        Cache.get(cacheKey, function(err, result) {
+            if (err) {
+                console.log(err);
+            }
+
+            if (result) {
+                reply(result);
+            }
+            else {
+                const result = db.prepare('SELECT * FROM treatments WHERE treatment_id = ?').get(request.params.id);
+
+                Cache.put(cacheKey, result, function(err) {
+                    if (err) {
+                        console.log(err);
+                    }
+
+                    reply(result);
+                });
+            }
+        });
+    }
+};
+
+module.exports = treatment;
