90b714dd98f6a69cffcb62a7e191a7bc96073dc1
Marcus Guidoti <marcus.guidoti@gmail.com>
2019-05-31 20:28:55 -0300

    modifies directories in config file

--- bin/truebug/config/default.js
+++ bin/truebug/config/default.js
@@ -4,8 +4,8 @@ module.exports = {
 
     "paths": {
         "timestampDir": path.join('.', 'download', 'timestamp.txt'),
-        "newTreatmentsDir": path.join('..', '..', 'data', 'treatmentsNew2'), 
-        "treatmentsListDir": path.join('..', '..', 'data', 'seach.xml')
+        "newTreatmentsDir": path.join('..', '..', 'data', 'treatmentsNew3'), 
+        "treatmentsListDir": path.join('..', '..', 'data')
     },
 
     "URLs": {
